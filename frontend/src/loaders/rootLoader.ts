d0e1914bc98afb279cde2cb3f66555da499e0641
outang <laurentd.dev@gmail.com>
2025-10-31 15:28:03 +0100

    remove react front

--- frontend/src/loaders/rootLoader.ts
+++
@@ -1,7 +0,0 @@
-import workspaceApi from "@/apis/workspace.api";
-import notificationApi from "@/apis/notification.api.ts";
-
-export const rootLoader = async () => {
-  const data = await Promise.all([workspaceApi.getWorkspaces(), notificationApi.getNotifications()]);
-  return { workspaces: data[0], notifications: data[1] };
-};
--- frontend/src/types/AuthUser.ts
+++
@@ -1,9 +0,0 @@
-export type AuthUser = {
-  id: string;
-  email: string;
-  name: string;
-  image?: string | null;
-  emailVerified: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-};
